d6578ddca67ba2f02924939a91f6eb4d11d52ec6
Mattia Barrasso <mattia.barrasso>
2026-03-07 11:43:14 +0100

    chore(scaffold): using start.spring.io, scaffold project

---
+++ src/main/java/com/ttia/pokepedia/PokepediaApplication.java
@@ -0,0 +1,13 @@
+package com.ttia.pokepedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokepediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokepediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ttia/pokepedia/PokepediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttia.pokepedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokepediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
