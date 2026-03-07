e92b8f95695f10f0523aaf109373def91fa97f81
Mattia Barrasso <mattia.barrasso>
2026-03-07 11:52:36 +0100

    test: add integration test and pre-commit hook

--- src/test/java/com/ttia/pokepedia/PokepediaApplicationTests.java
+++ src/test/java/com/ttia/pokepedia/PokepediaApplicationTests.java
@@ -2,12 +2,24 @@ package com.ttia.pokepedia;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class PokepediaApplicationTests {
+
+	@LocalServerPort
+	private int port;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void serverStartsOnRandomPort() {
+		assertThat(port).isPositive();
 	}
 
 }
